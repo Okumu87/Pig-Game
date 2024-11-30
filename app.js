001c88bbd3e594d97b7a18e2a9ca341ae91edfce
okumu87 <okumu793@gmail.com>
2024-11-30 19:29:07 +0100

    Added functionality on dice

--- app.js
+++ app.js
@@ -1,1 +1,35 @@
 "use strict ";
+
+// Selecting elements
+const score0El = document.getElementById("score--0");
+const score1El = document.getElementById("score--1");
+// Scores
+const current0El = document.getElementById("current--0");
+const current1El = document.getElementById("current--1");
+// dice
+const diceEl = document.querySelector(".dice");
+// Buttons
+const btnNew = document.querySelector(".btn--new");
+const btnRoll = document.querySelector(".btn--roll");
+const btnHold = document.querySelector(".btn--hold");
+
+// Starting conditions
+
+score0El.textContent = 0;
+score1El.textContent = 0;
+diceEl.classList.add("hidden");
+
+// Stored values
+
+let currentScore = 0;
+
+// Rolling dice functionality
+btnRoll.addEventListener("click", function () {
+  // 1. Generating a random dice roll
+  const dice = Math.trunc(Math.random() * 6) + 1;
+  console.log(dice);
+  // 2. Display dice
+  diceEl.classList.remove("hidden");
+  diceEl.src = `dice-${dice}.png`;
+  // 3. Check for rolled 1
+});
