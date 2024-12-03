4a4f9ffe2cca86ec414d46209c7320dd9ad5ba01
okumu87 <okumu793@gmail.com>
2024-12-03 15:52:43 +0100

    Added toggle when player wins

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 "use strict ";
 
 // Selecting elements
+// playerMode
+const player0El = document.querySelector(".player--0");
+const player1El = document.querySelector(".player--1");
+// Scores
 const score0El = document.getElementById("score--0");
 const score1El = document.getElementById("score--1");
-// Scores
+
 const current0El = document.getElementById("current--0");
 const current1El = document.getElementById("current--1");
 // dice
@@ -19,6 +23,7 @@ score1El.textContent = 0;
 diceEl.classList.add("hidden");
 
 // Stored values
+const scores = [0, 0];
 let currentScore = 0;
 let activePlayer = 0;
 
@@ -34,8 +39,14 @@ btnRoll.addEventListener("click", function () {
   if (dice !== 1) {
     // Add dice to current score
     currentScore += dice;
-    current0El.textContent = currentScore; //Change to current player later
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
   } else {
     // Switch to next player
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    currentScore = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    player0El.classList.toggle("player--active");
+    player1El.classList.toggle("player--active");
   }
 });
